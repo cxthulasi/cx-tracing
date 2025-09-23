e955aa820baef410232a6db4fe69d12addceeba7
ramthulsi <thulasiram.toorpu@coralogix.com>
2025-09-23 08:22:02 +0530

    updated grpc instead of http

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const { NodeSDK } = require('@opentelemetry/sdk-node');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-proto");
+const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-grpc");
 const { Resource } = require('@opentelemetry/resources');
 const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 const opentelemetry = require('@opentelemetry/api');
